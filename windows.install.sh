33055e5a8087f3fa32110d9174a53b8315295981
Benney Au <benney.au@prospa.com>
2020-02-19 23:19:41 +1100

    update windows install

--- windows.install.sh
+++ windows.install.sh
@@ -4,7 +4,8 @@ git submodule update --init --recursive
 # vim
 cp -f ~/dotfiles/.vimrc ~/.vimrc
 cp -f ~/dotfiles/.gvimrc ~/.gvimrc
-cp -fr ~/dotfiles/.vim/* ~/vimfiles
+cp -rf ~/dotfiles/.vim/* ~/vimfiles
+cp -rf ~/dotfiles/documents ~/
 # also copy to .vim folder so that git bash works too
 cp -fr ~/dotfiles/.vim/* ~/.vim
 cp -f ~/dotfiles/.bashrc.win ~/.bashrc
