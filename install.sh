41a38f3881128ca0ab4c58fc8f2a8e86b4bdb29a
Benney Au <benney_au@hotmail.com>
2024-11-19 02:36:55 +1100

    feat(vim): add lvim configuration

--- install.sh
+++ install.sh
@@ -9,3 +9,4 @@ ln -s ~/dotfiles/.bashrc ~/.bashrc
 ln -s ~/dotfiles/.inputrc ~/.inputrc
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/.config/lvim/config.lua ~/.config/lvim/config.lua
